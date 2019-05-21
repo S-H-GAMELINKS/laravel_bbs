a3fc7ef941534a46e3848d401d15f23bb3ccc512
S-H-GAMELINKS <gamelinks007@gmail.com>
2019-05-21 16:43:55 +0900

    add Scaffold

--- config/app.php
+++ config/app.php
@@ -175,6 +175,8 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
+        // Add Scaffold
+        Laralib\L5scaffold\GeneratorsServiceProvider::class,
     ],
 
     /*
